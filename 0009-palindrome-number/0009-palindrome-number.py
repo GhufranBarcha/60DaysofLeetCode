587a573d29cea0479b5d36471eecaf7ad79459d4
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-07-04 21:02:18 +0500

    Time: 61 ms (27.46%), Space: 16.6 MB (38.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,6 +3,17 @@ class Solution:
         if x < -2**31 or x > 2**31-1:
             return 0
 
+        if x < 0 :
+            return False
+        else:
+            reverse = 0
+            x_1 = x
+
+            while x > 0:
+                reverse = (reverse * 10 ) + (x % 10)   
+                x = x // 10
+            return reverse == x_1         
+
         if str(x) == str(x)[::-1]:
             return True
         else:
