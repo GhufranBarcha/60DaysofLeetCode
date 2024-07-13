69a27a77f9e82c2e1b455799929b0aadaaa4493c
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-07-13 17:46:09 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) > 1 and k > len(nums):
+            nums[0] ,nums[1]=nums[1],nums[0]
+        else:    
+            mid = len(nums) - k
+            arr = nums[mid:] + nums[:mid]
+
+            for i in range(len(nums)):
+                nums[i] = arr[i]
+        
