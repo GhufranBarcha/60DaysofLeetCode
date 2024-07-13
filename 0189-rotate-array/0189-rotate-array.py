1da4e4cd20c4a9ed52135210ded7b41e3301eac4
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-07-13 19:21:37 +0500

    Time: 151 ms (83.85%), Space: 27.8 MB (78.5%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,25 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # if len(nums) == 2 and k > len(nums):
-        #     nums[0],nums[1]=nums[1],nums[0]
-        # else:    
-        #     mid = len(nums) - k
-        #     nums[:] = nums[mid:] + nums[:mid]
-        for _ in range(k):
-            nums = self.rotate1(nums)
+        if len(nums) == 2 and k > len(nums):
+            nums[0],nums[1]=nums[1],nums[0]
+        else:    
+            mid = len(nums) - k
+            nums[:] = nums[mid:] + nums[:mid]
+        # for _ in range(k):
+        #     nums = self.rotate1(nums)
 
         
-    def rotate1(self,nums):
-            temp = nums[len(nums)-1]
-            for i in range(len(nums) -1 ,0,-1):
-                print(i)
+    # def rotate1(self,nums):
+    #         temp = nums[len(nums)-1]
+    #         for i in range(len(nums) -1 ,0,-1):
+    #             print(i)
                 
-                print("temp",temp)
-                nums[i] = nums[i-1]
+    #             print("temp",temp)
+    #             nums[i] = nums[i-1]
                 
-            nums[0] = temp 
-            return nums
+    #         nums[0] = temp 
+    #         return nums
        
 
 
