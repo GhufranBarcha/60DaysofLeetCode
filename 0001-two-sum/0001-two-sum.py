01874a4b1096a319459ed945f6d7921245689b0a
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-07-16 20:58:49 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,19 +2,35 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         ## Trying optimal way( Does't work as we cant modify array)
-        numMap = {}
-        n = len(nums)
+        num = sorted(nums)
+        i = 0
+        j = 1
+        store = [1 ,1]
 
-        for i in range(n):
-            numMap[nums[i]] = i
+        while True:
+            if num[j] > target:
+                i = None
+                j = None
+                break
+            if num[i] + num[i+j] > target:
+                j+=1
+                i = 0
+            if num[i]+num[i+j] == target:
+                print(i ,i+j)
+                break
 
-        ## Find the complement:
+            i+=1
+            
+        first = True
+            
+        for f in range(len(num)):
+            if num[i] == nums[f] and first:
+                first = False
+                store[0] = f
+            if num[i+j] == nums[f]:
+                store[1] = f   
 
-        for i in range(n):
-            result = target - nums[i]
-            if result in numMap and numMap[result] != i:
-                return [i, numMap[result]]  
-      
+        return store       
 
 
 
