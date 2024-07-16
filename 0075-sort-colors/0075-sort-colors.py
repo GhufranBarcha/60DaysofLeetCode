33cc4452c4fc971bde2be38f7d53ca6c0158142d
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-07-17 00:27:40 +0500

    Time: 31 ms (90.96%), Space: 16.4 MB (92.93%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,15 +4,33 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        ## Brute Force Method
+        ## Optimized using one pass Dutch Flag
 
-        zero = nums.count(0)
-        one = nums.count(1)
-        two = nums.count(2)
+        low = 0
+        mid = 0
+        high = len(nums)-1
 
-        num = ([0]*zero) + ([1]*one) + ([2]*two)
-        nums[:] = num[:]
-        return nums
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low],nums[mid] = nums[mid] ,nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid] == 1:
+                mid+=1
+            else:
+                nums[mid],nums[high] = nums[high],nums[mid]
+                high-=1        
+
+
+        # ## Brute Force Method
+
+        # zero = nums.count(0)
+        # one = nums.count(1)
+        # two = nums.count(2)
+
+        # num = ([0]*zero) + ([1]*one) + ([2]*two)
+        # nums[:] = num[:]
+        # return nums
 
        
 
