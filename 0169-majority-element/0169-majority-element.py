48396f544461b9356da8b8351d94b5c25d095c58
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-07-17 00:41:07 +0500

    Time: 163 ms (86.52%), Space: 18 MB (56.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,57 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+
+        ## Brute force approch 
+        ## just sort the array and find the middle point
+
+        nums.sort()
+        n = nums[len(nums)//2]
+        return n
+
+        ## Boyer-Moore Majority voting algorith
+
+        n = 0
         count = 0
-        candidate =0
 
         for num in nums:
             if count == 0:
-                candidate = num
+                n = num
+            elif n != num:
+                count -=1   
 
-            if num == candidate:
-                count+=1
-            else:
-                count -=1
-        return candidate                
+        return n        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # count = 0
+        # candidate =0
+
+        # for num in nums:
+        #     if count == 0:
+        #         candidate = num
+
+        #     if num == candidate:
+        #         count+=1
+        #     else:
+        #         count -=1
+        # return candidate                
         
 
         
