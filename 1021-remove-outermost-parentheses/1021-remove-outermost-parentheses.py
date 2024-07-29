7f2c43ed049d3601b6b141eff942ebf56de931e8
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-07-29 21:58:20 +0500

    Time: 41 ms (42%), Space: 16.5 MB (78.68%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,32 +1,46 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        left_paran = 0
-        right_paran = 0
-        count = []
-        st = []
-
-        for index,i in enumerate(s):
-            if left_paran == right_paran:
-                left_paran = 0
-                right_paran = 0
-                count.append(index)
-        
-            if i == "(":
-                left_paran +=1
+        result = []
+        count = 0
+        for char in s:
+            if char == "(":
+                if count > 0:
+                    result.append(char)
+                count +=1
             else:
-                right_paran += 1
+                count -= 1
+                if count > 0:  
+                    result.append(char)   
+        return "".join(result)            
+
+        ## My solution
+        # left_paran = 0
+        # right_paran = 0
+        # count = []
+        # st = []
+
+        # for index,i in enumerate(s):
+        #     if left_paran == right_paran:
+        #         left_paran = 0
+        #         right_paran = 0
+        #         count.append(index)
+        
+        #     if i == "(":
+        #         left_paran +=1
+        #     else:
+        #         right_paran += 1
                 
-            if left_paran == right_paran:
-                left_paran = 0
-                right_paran = 0
-                count.append(index)        
-        for index,i in enumerate(s):
-            if index not in count:
-                st.append(i)
+        #     if left_paran == right_paran:
+        #         left_paran = 0
+        #         right_paran = 0
+        #         count.append(index)        
+        # for index,i in enumerate(s):
+        #     if index not in count:
+        #         st.append(i)
                 
-        st = ''.join(st)        
+        # st = ''.join(st)        
                 
-        return st
+        # return st
             
 
         
