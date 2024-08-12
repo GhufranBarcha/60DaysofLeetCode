4c943735c7e5fc875bb327cfe5ced8f975d15079
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-08-12 22:50:12 +0500

    Time: 1311 ms (62.57%), Space: 16.6 MB (31.69%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,15 +1,29 @@
 from collections import Counter
 class Solution:
     def beautySum(self, s: str) -> int:
-        sum = 0
+        res = 0
         for i in range(len(s)):
-            for j in range(i ,len(s)+1):
-                if len(s[i:j]) > 2:
-                    char_count = Counter(s[i:j])
-                    max_char = max(char_count.values())
-                    min_char = min(char_count.values())
-                    sum += max_char - min_char
-        return sum    
+            count = defaultdict(int)
+            for j in range(i , len(s)):
+                count[s[j]] +=1
+                minF = min(count.values())
+                maxF = max(count.values())
+
+                res += maxF -  minF
+        return res        
+
+
+        ## Works but not optimized
+        # sum = 0
+        # for i in range(len(s)):
+        #     for j in range(i ,len(s)+1):
+                
+        #         if len(s[i:j]) > 2:
+        #             char_count = Counter(s[i:j])
+        #             max_char = max(char_count.values())
+        #             min_char = min(char_count.values())
+        #             sum += max_char - min_char
+        # return sum    
               
 
 
