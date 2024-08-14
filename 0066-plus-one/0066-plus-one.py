770dff2862a98a8d5cec5d365f9527b2b2a686ce
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-08-14 19:50:21 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,28 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = int("".join(map(str,digits))) + 1
-        n = [int(i) for i in str(num)]
 
-        return n
+        i = len(digits) - 1
+        for i in range(len(digits)-1 , -1 ,-1):
+            if digits[i] != 9:
+                digits[i] += 1
+                break
+            elif digits[i] == 9:
+                digits[i] == 0
+            elif digits[i] == 9 and i == 0:
+                digits[i] == 1
+                digits.append(0)
+
+        if len(digits) == 1:
+            if digits[0] == 9:
+                digits[0] = 1
+                digits.append(0)
+            else:
+                digits[0] += 1    
+        return digits        
+        
+
+
+        ## First approch
+        # num = int("".join(map(str,digits))) + 1
+        # n = [int(i) for i in str(num)]
+        # return n
