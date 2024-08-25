ef0c54b806d17aeaa459b42486de1a3c8101f85b
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-08-25 23:38:49 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        if not head:
+            return None  
+        arr = []    
+        temp = head
+        arr.append(temp.val)
+        
+
+        while True:
+            
+            temp = temp.next
+            arr.append(temp.val)
+            if not temp.next:
+                break
+
+        if arr == arr[::-1]:
+            return True
+        else:
+            return False        
+
+
+
+
+        
