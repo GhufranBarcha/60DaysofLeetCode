13f12ba1d7ace87c273c8e36c6e5d47c9a82b1ac
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-09-12 01:23:11 +0500

    Time: 18 ms (63.21%), Space: 11.7 MB (31.28%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,50 +1,44 @@
-class Solution:
-    def myAtoi(self, s: str) -> int:
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """ 
         s = s.strip()
         if s == "":
-            return  0
+            return 0
+
         start = None
         result = 0
+
         if s[0] == "+" or s[0] == "-":
             start = s[0]
             s = s[1:]
 
-        for i in range(1,len(s)+1):
+        for i in range(1 , len(s) + 1):
             try:
                 if start != None:
                     result = int(start + s[0:i])
                 else:
-                    result = int(s[0:i])
-               
+                    result = int(s[0:i])  
             except:
-                break
+                break   
+                 
         if result < -2**31 and start != None:
-                result = -2**31
-                return result  
+            result = -2**31
+            return result  
         if result > 2**31 - 1:
-                result = 2**31 - 1
-                return result              
-        return result        
-
-     
+            result = 2**31 - 1
+            return result              
+        return result             
 
 
 
 
 
-        # sign = None
 
 
 
-        # if s[0] == "+" or s[0] == "-":
-        #     sign = s[0]
-        # num = None    
-
-        # for i in range(1 ,len(s)):
-        #     if s[i] == type("int"):
-        #         if s[i] == 0 and num is not None:
-        #             num = 0
-        #             return num
 
 
             
