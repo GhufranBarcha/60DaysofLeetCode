6548229ff172847c14057580059ce89cc1c9138f
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2024-10-29 22:59:36 +0500

    Time: 36 ms (57.05%), Space: 16.6 MB (16.34%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,15 +1,51 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x <= -2**31 or x>= 2**30:
-            return 0
-        if x >= 0:
-            str_x = str(x)
-            str_x = str_x[::-1]
-            return int(str_x)
-        else:
-            x = abs(x)
-            str_x = str(x)
-            str_x = str_x[::-1]
-            return int(str_x)*(-1)
+        val = 0
 
+        sign = 1 if x > 0 else -1 
+        x = abs(x)
+
+        while x:
+
+            div = x % 10
+            val = (val * 10) + div
+            x = x // 10
+
+
+        # Apply the sign
+        val *= sign
         
+        # Check 32-bit overflow
+        if val < -2**31 or val > 2**31 - 1:
+            return 0
+        
+        return val
+
+    
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+        ## Brute force approch
+        # if x >= 0:
+        #         num = int(str(x)[::-1])
+        #         return num if num <= (2**31) and num >= -2**31 else 0
+        # else:
+                
+        #         value = str(x)[1:]
+        #         value = int(value[::-1])
+        #         num = value * -1
+        #         return num if num <= (2**31) and num >= -2**31 else 0
+
+
+
+            
