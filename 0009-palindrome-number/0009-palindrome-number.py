254c112cda3448e1f1ae6de149930fbfe89247d4
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2026-02-12 00:05:02 +0500

    Time: 11 ms (37.46%), Space: 19.3 MB (54.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,5 +1,29 @@
+
+## Easy solution( convert to string , flip it and match the string) 
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         pal = str(x) == str(x)[::-1]
+#         return pal
+        
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        pal = str(x) == str(x)[::-1]
-        return pal
+        if x < 0:
+            return False
+
+        copy = x    
+
+        reverse = 0
+
+
+        while x > 0:
+            remainder = x % 10
+            reverse = (reverse * 10) + remainder
+            x = x // 10
+
+
+        return  reverse == copy
+        
+
+
+
         
